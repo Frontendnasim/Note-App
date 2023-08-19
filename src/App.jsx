ccb57fc462b4f8dacc62d48087d301b6f784b604
nasim shiroodi <nasim.breeze97@gmail.com>
2023-08-19 14:54:00 +0330

    Create Note App with react.js

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import "./App.css";
+import AddNewNote from "./components/AddNewNote";
+import NoteList from "./components/NoteList";
+import NotesStatus from "./components/NotesStatus";
+import NoteHeader from "./components/NoteHeader";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+  const [sortBy, setSortBy] = useState("latest");
+  const handleAddNote = (newNote) => {
+    setNotes((prevNotes) => [...prevNotes, newNote]);
+  };
+
+  const handleDeleteNote = (id) => {
+    // const filteredNotes = notes.filter((n) => n.id !== id);
+    // setNotes(filteredNotes);
+    setNotes((prevNotes) => prevNotes.filter((n) => n.id !== id));
+  };
+
+  const handleCompleteNote = (e) => {
+    const noteId = Number(e.target.value);
+    // const newNotes = notes.map((note) =>
+    //   note.id === noteId ? { ...note, completed: !note.completed } : note
+    // );
+    // setNotes(newNotes);
+
+    setNotes((prevNotes) =>
+      prevNotes.map((note) =>
+        note.id === noteId ? { ...note, completed: !note.completed } : note
+      )
+    );
+  };
+
+  return (
+    <div className="container">
+      <NoteHeader
+        notes={notes}
+        sortBy={sortBy}
+        onSort={(e) => setSortBy(e.target.value)}
+      />
+      <div className="note-app">
+        <AddNewNote onAddNote={handleAddNote} />
+        <div className="note-container">
+          <NotesStatus notes={notes} />
+          <NoteList
+            notes={notes}
+            sortBy={sortBy}
+            onDelete={handleDeleteNote}
+            onComplete={handleCompleteNote}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NotesStatus.jsx
@@ -0,0 +1,30 @@
+import Message from "./Message";
+
+function NotesStatus({ notes }) {
+  const allNotes = notes.length;
+  const completedNotes = notes.filter((n) => n.completed).length;
+  const unCompletedNotes = allNotes - completedNotes;
+
+  if (!allNotes)
+    return (
+      <Message>
+        ⚠️ <span>No Notes has already been added.</span>
+      </Message>
+    );
+
+  return (
+    <ul className="note-status">
+      <li>
+        All <span>{allNotes}</span>
+      </li>
+      <li>
+        Completed <span>{completedNotes}</span>
+      </li>
+      <li>
+        Open <span>{unCompletedNotes}</span>
+      </li>
+    </ul>
+  );
+}
+
+export default NotesStatus;
